1e7aea604430841de189e3436d39916cbb19c518
DanielBoomer <daniel.mcclain7@gmail.com>
2017-10-21 13:05:26 -0400

    Index team members and push notifications through Slack

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const http = require('http');
 
+var WebClient = require('@slack/client').WebClient;
+
+var token = process.env.SLACK_API_TOKEN || '';
+
+var web = new WebClient(token);
+
 // Initialize using verification token from environment variables
 const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
 const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
@@ -26,14 +32,17 @@ app.use('/slack/events', slackEvents.expressMiddleware());
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('reaction_added', (event)=> {
   if (ifReactionApplicable(event.reaction, event.item.channel)) {
-    console.log(`Received an added reaction event: user ${event.user} in channel ${event.item.channel} reaction: ${event.reaction}`);
-    console.log(getNextTeamMember())
+    let teamMember = getNextTeamMember();
+    let message = `@${teamMember} YOU'RE UP!`;
+    sendThreadedMessage(message, event.item.channel, event.item.ts);
   }
 });
 slackEvents.on('reaction_removed', (event)=> {
   if (ifReactionApplicable(event.reaction, event.item.channel)) {
-    console.log(`Received a removed reaction event: user ${event.user} in channel ${event.item.channel} reaction: ${event.reaction}`);
-    console.log(getPreviousTeamMember())
+    let teamMember = getCurrentTeamMember();
+    index -= 1;
+    let message = `@${teamMember} YOU GET A SECOND LIFE`;
+    sendThreadedMessage(message, event.item.channel, event.item.ts);
   }
 });
 
@@ -58,4 +67,16 @@ function getPreviousTeamMember() {
 }
 function getCurrentTeamMember() {
   return TEAM[index % TEAM.length];
+}
+function sendThreadedMessage(message, channel, ts) {
+  web.chat.postMessage(channel, message, {
+    thread_ts: ts,
+    parse: 'full'
+  }, function(err, res) {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      console.log('Message sent!');
+    }
+  });
 }
