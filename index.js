9886e9bbcf04c2a15e7dd7762498f561fac5d063
DanielBoomer <daniel.mcclain7@gmail.com>
2017-10-21 11:28:11 -0400

    Added a basic reaction events listener

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+
+// Initialize using verification token from environment variables
+const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+const port = process.env.PORT || 3000;
+
+// Initialize an Express application
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// You must use a body parser for JSON before mounting the adapter
+app.use(bodyParser.json());
+
+// Mount the event handler on a route
+// NOTE: you must mount to a path that matches the Request URL that was configured earlier
+app.use('/slack/events', slackEvents.expressMiddleware());
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('reaction_added', (event)=> {
+  console.log(`Received an added reaction event: user ${event.user} in channel ${event.item.channel} reaction: ${event.reaction}`);
+});
+slackEvents.on('reaction_removed', (event)=> {
+  console.log(`Received a removed reaction event: user ${event.user} in channel ${event.item.channel} reaction: ${event.reaction}`);
+});
+
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
+
+// Start the express application
+http.createServer(app).listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
