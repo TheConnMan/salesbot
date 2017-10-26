1a91d7ae288ea9b307cdfe9f92601d2ed7266448
DanielBoomer <daniel.mcclain7@gmail.com>
2017-10-26 14:53:29 -0400

    Implemented Slack slash commands to display next team member in line

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var web = new WebClient(token);
 const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
 const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
 const port = process.env.PORT || 3000;
+const VERIFY_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
 
 // Initialize an Express application
 const express = require('express');
@@ -24,6 +25,10 @@ let index = 0;
 
 // You must use a body parser for JSON before mounting the adapter
 app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Mount the event handler on a route
 // NOTE: you must mount to a path that matches the Request URL that was configured earlier
@@ -63,6 +68,27 @@ app.get('/get', function (req, res) {
   res.send(getCurrentTeamMember());
 });
 
+// Handling slah commands in Slack
+app.get('/slack', function (req, res) {
+  res.sendStatus(200)
+})
+
+app.post(bodyParser.urlencoded({extended:true}), function(req, res) {
+  if (req.body.token !== VERIFY_TOKEN) {
+    return res.sendStatus(401)
+  }
+
+  if (req.body.text === 'whosnext') {
+      res.json({
+        text: TEAM[(index + 1) % TEAM.length]
+      });
+  } else {
+    res.json({
+      text: 'I need valid instructions like "whosnext"'
+    });
+  }
+})
+
 // Start the express application
 http.createServer(app).listen(port, () => {
   console.log(`server listening on port ${port}`);
@@ -81,6 +107,9 @@ function getPreviousTeamMember() {
 }
 function getCurrentTeamMember() {
   return TEAM[index % TEAM.length];
+}
+function listCurrentTeamMember() {
+  return TEAM[(index + 1) % TEAM.length];
 }
 function sendThreadedMessage(message, channel, ts) {
   web.chat.postMessage(channel, message, {
